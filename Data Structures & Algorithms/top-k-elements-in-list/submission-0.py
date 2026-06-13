ab98225b5d2087d0233446f6cd02875b10c0f877
sgrkumards <sgrkumards@gmail.com>
2026-06-13 15:45:03 +0100

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = defaultdict(int)
+        for num in nums:
+            freq[num] += 1
+
+        return sorted(freq.keys(),key = lambda x: freq[x], reverse =True )[:k]
+
