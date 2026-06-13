27c711b1b70637a6cabd60e18534336738a1324e
sgrkumards <sgrkumards@gmail.com>
2026-06-13 21:01:54 +0100

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = [c.lower() for c in s if c.isalnum()]
+        # print(cleaned)
+        left = 0 
+        right = len(cleaned) -1 
+        while left < right:
+            if cleaned[left] == cleaned[right]:
+                left += 1
+                right -= 1
+            else:
+                return False 
+        return True 
