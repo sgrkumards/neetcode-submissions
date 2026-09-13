1e9359b1d49595e8e043d41485cbc71b28d0fa1c
sgrkumards <sgrkumards@gmail.com>
2026-09-13 15:43:32 +0100

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 10
+float_type = 9.1
+bool_type = True
+string_type =" Zeo"
+list_type =[1,2,'a','c']
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
