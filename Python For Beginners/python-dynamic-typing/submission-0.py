b517896741185b52ccce79b8dff874fec561ee15
sgrkumards <sgrkumards@gmail.com>
2026-09-13 15:45:10 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 90.0
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Ethan"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
